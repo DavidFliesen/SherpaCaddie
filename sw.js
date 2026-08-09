faf21ce650303f2da74f7500343053d231a76aed
David Fliesen <dfliesen@gmail.com>
2026-08-08 22:23:48 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,32 @@
-const CACHE='sherpa-caddie-v2.1';const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/favicon.png','./assets/sherpa-caddie-logo.jpg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
+
+const CACHE = "sherpa-caddie-v0.3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./assets/sherpa-caddie-logo-clean.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/favicon.png"
+];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request).then(networkRes => {
+      const copy = networkRes.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(()=>{});
+      return networkRes;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
